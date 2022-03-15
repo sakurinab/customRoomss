e5a91874f236af540eb34559275d4d3f9a764ca2
sakurinab <93381089+sakurinab@users.noreply.github.com>
2022-03-15 11:39:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
-module.exports.token = 'OTQwNTU4NTc3MDQ1NzQxNTk4.YgJJbw.Ziu4ZqgyYum9dWY-8K7UqKu-wr8'
+module.exports.token = 'OTUzMTk2NDg3NjI4Njg5NDI4.YjBDaA.KgGv-0IAd_6hKedIqV4hnL8asJA'
 
-module.exports.adminID = '921741284824211486'
+module.exports.adminID = '895354757860978728'
 
 module.exports.dataURL = 'mongodb+srv://suka:7j5kl3dk@aisha.ii1g0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 module.exports.botId = '940558577045741598'
 
-module.exports.mainGuild = "939173127424991293"
+module.exports.mainGuild = "940536267861544960"
 
-module.exports.marryCost = '1000'
-
-module.exports.loveParent = '940539417007898685'
-
-module.exports.everyoneID = "939173127424991293"
-
+module.exports.everyoneID = "940536267861544960"
